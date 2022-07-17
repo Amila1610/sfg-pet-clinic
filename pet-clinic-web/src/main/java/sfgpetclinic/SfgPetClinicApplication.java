1b66f7552915b9a47621c2b5067f9e0fde2ca62e
Amila1610 <amilahasovic@hotmail.com>
2022-07-17 17:07:13 +0200

    refactoring to multimodule maven project. closes #1

--- pet-clinic-web/src/main/java/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,11 +1,10 @@
-package guru.springframework.sfgpetclinic;
+package sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
