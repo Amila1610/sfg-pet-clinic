354363513418e91a3b7d6de0c16049910b75b5b4
Amila1610 <amilahasovic@hotmail.com>
2022-08-01 16:26:25 +0200

    adding JPA Repositories. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public interface SpecialtyRepository extends CrudService<Speciality,Long> {
+}
